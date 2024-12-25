e39cade3aa66a7346d390405ce3820bb709b0f62
Rashim <rashimrb22@gmail.com>
2024-12-25 20:19:46 +0530

    ip-fix

--- src/controllers/ipController.js
+++ src/controllers/ipController.js
@@ -1,6 +1,6 @@
 const getIpAddress = (req, res) => {
     console.log("GET /ip");
-    const ip = req.ip;
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     console.log(`User IP Address: ${ip}`);
     res.send({ ip: ip });
 };
